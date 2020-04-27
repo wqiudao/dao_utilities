7913fd6e46d37faa4ee6f23998ef8adee166f866
wqiudao <33651085+wqiudao@users.noreply.github.com>
2020-04-27 18:57:25 +0800

    Update match_gene.py

--- match_gene.py
+++ match_gene.py
@@ -3,7 +3,7 @@
 # gencode.v23.annotation.gene.probemap  注释文件，可以替换
 # id	gene	chrom	chromStart	chromEnd	strand
 # ENSG00000223972.5	DDX11L1	chr1	11869	14409	+
-# python match_gene.py Pancreas_TcgaTargetGTEX.tsv 
+# python match_gene.py Pancreas_TcgaTargetGTEX.tsv   
 
 import re
 import sys
@@ -18,7 +18,9 @@ reads=input_file. readline()
 while reads:
 	reads=re.sub(r'[\r\n]+','',reads)
 	readset=re.split("\s+",reads) 	
+	readset[1]="\'"+readset[1]
 	geneId=re.sub(r'\.\S+','',readset[0])
+	reads="\t".join(readset)
 	dic_geneAnnotation[geneId]=reads
 	reads=input_file. readline()
 input_file.close()
@@ -27,6 +29,7 @@ print("Loading map file done...")
 
 filename=sys.argv[1];input_file =open(filename)
 opfname=re.sub(r'[\.\_]+[a-zA-Z0-9]+$','_Annotation.tsv',sys.argv[1]);opt_file =open(opfname,"w")
+opfname=re.sub(r'[\.\_]+[a-zA-Z0-9]+$','_Sel_Annotation.tsv',sys.argv[1]);opts_file =open(opfname,"w")
 
 reads=input_file. readline()
 reads=re.sub(r'[\r\n]+','',reads)
@@ -34,10 +37,15 @@ readset=re.split("\s+",reads)
 geneId=re.sub(r'\.\S+','',readset[Id_geneLocation])
 opt_file.write(reads)
 if geneId in dic_geneAnnotation:
+	opts_file.write(reads)
+	opts_file.write("\t"+dic_geneAnnotation[geneId])
 	opt_file.write("\t"+dic_geneAnnotation[geneId])
 else:
+	opts_file.write(reads)
 	opt_file.write("\tid\tgene\tchrom\tchromStart\tchromEnd\tstrand")
+	opts_file.write("\tid\tgene\tchrom\tchromStart\tchromEnd\tstrand")
 opt_file.write("\n")
+opts_file.write("\n")
 
 reads=input_file. readline()
 while reads:
@@ -47,20 +55,14 @@ while reads:
 	opt_file.write(reads)
 	if geneId in dic_geneAnnotation:
 		opt_file.write("\t"+dic_geneAnnotation[geneId])
+		opts_file.write(reads)
+		opts_file.write("\t"+dic_geneAnnotation[geneId])
+		
 	opt_file.write("\n")
+	opts_file.write("\n")
 	reads=input_file. readline()
 input_file.close()
 opt_file.close()
+opts_file.close()
 
 print("Mapping done...")
-
-
-
-
-
-
-
-
-
-
-
